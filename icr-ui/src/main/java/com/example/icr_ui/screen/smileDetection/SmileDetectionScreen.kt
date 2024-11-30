8052759354d7ef5cf2f83556b81a5ae05d187c3a
Mahmoud <mahmoudkamal4673@gmail.com>
2024-12-01 01:49:42 +0200

    feat(ui): initialize camera screen design with circular preview and error handling

--- icr-ui/src/main/java/com/example/icr_ui/screen/smileDetection/SmileDetectionScreen.kt
+++ icr-ui/src/main/java/com/example/icr_ui/screen/smileDetection/SmileDetectionScreen.kt
@@ -1,16 +1,38 @@
 package com.example.icr_ui.screen.smileDetection
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.example.icr_ui.components.ICRCircularCameraPreview
 
 @Composable
 fun SmileDetectionScreen() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        ICRCircularCameraPreview { bitmap ->
+    val context = LocalContext.current
+    var showCamera by remember { mutableStateOf(false) }
 
+    val cameraPermissionLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission(),
+            onResult = { isGranted: Boolean ->
+                showCamera = isGranted
+            })
+
+    val permissionCheck = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+    LaunchedEffect(permissionCheck) {
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
+    }
+
+    ICRCircularCameraPreview { bitmap ->
+
     }
 }
